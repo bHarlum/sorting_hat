1038719ea016e00073c600b94b836fd8326a97f9
Bryce harlum <bryce.harlum@gmail.com>
2019-04-10 11:06:05 +1000

    Added a new implemented the show method, new file: show.html.erb within view folder. Displays data of specified ID. Implemented next and previous links to go to next and previous page if they exist.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
         house = houses[rand(0..2)]
         p params
         n_user[:name] = params[:user][:name]
-        n_user[:house] = params[:user][:house]
+        n_user[:house] = house
         n_user[:name] = params[:user][:name]
         n_user[:project] = params[:user][:project]
         n_user.save
@@ -17,6 +17,8 @@ class UsersController < ApplicationController
     def new
     end
     def show
+        @users = (User.all)
+        @user = User.find(params[:id])
     end
     def update
     end
